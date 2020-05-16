1e9c2c6980f3135e88cfde0cbc9fd44f7aedc955
devanshi2901 <65292135+devanshi2901@users.noreply.github.com>
2020-05-16 13:41:25 +0530

    Python code

---
+++ code5.py
@@ -0,0 +1,9 @@
+rp1=int(input("runs scored in 60 balls="))
+rp2=int(input("runs scored in 60 balls="))
+rp3=int(input("runs scored in 60 balls="))
+sr1=rp1*100/60
+sr2=rp2*100/60
+sr3=rp2*100/60
+print("strike rate of player 1= ",sr1,"player 2=",sr2,"player 3= ",sr3)
+print("runs scored if they played 60 more balls",rp1*2,rp2*2,rp3*2)
+print("no of sixes players hit",rp1//6,rp2//6,rp3//6)
