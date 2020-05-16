1425de8f343726d38ea33b4ef1cdffcd88721ab2
devanshi2901 <65292135+devanshi2901@users.noreply.github.com>
2020-05-16 15:32:34 +0530

    Day2

---
+++ code4day2.py
@@ -0,0 +1,12 @@
+for i in range(2):
+	n=3-i
+	print("\n")
+	for j in range(n):
+		print(n,"*",end=" ")
+print(n)
+print("1\n1")
+for i in  range(2,4):
+	print("\n")
+	for j in range(i-1):
+		print(i,"*",end="  ")
+print(i)
