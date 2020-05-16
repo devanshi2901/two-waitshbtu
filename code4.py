21de8f8791f646bdf275f8acc078e00cf5c075b2
devanshi2901 <65292135+devanshi2901@users.noreply.github.com>
2020-05-16 13:40:51 +0530

    Python code

---
+++ code4.py
@@ -0,0 +1,6 @@
+cp=float(input("enter the cost price="))
+sp=float(input("enter the selling price"))
+profit=sp-cp
+newsp=1.05*profit +cp
+print("profit is =",profit)
+print("new selling price",newsp)
