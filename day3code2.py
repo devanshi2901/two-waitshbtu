b5c382c6f8326881814a44258f2cda6802f89862
devanshi2901 <65292135+devanshi2901@users.noreply.github.com>
2020-05-16 17:58:22 +0530

    Day3

---
+++ day3code2.py
@@ -0,0 +1,15 @@
+def permutation(List,String=" ")
+Set=set(List)
+stringList=[ ]
+if len(Set)==1 :
+String +=" ".join(List)
+return list([String])
+for x in Set:
+List1=list(List)
+S=String + x
+List1.remove(x)
+stringlist.extend(permutation(List1,S))
+return stringlist
+string=input("Enter the string:")
+List=permutation(list(string))
+print("All the possible permutation for the givenstring is : "+",".join(List))
